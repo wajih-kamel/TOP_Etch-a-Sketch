eab5410f4d0047d9b5f80e390cb73bafd937c687
wajih-kamel <wajihfb@gmail.com>
2025-08-30 01:57:51 +0100

    adding grid mouseover event

--- Etch-a-Sketch.js
+++ Etch-a-Sketch.js
@@ -1,3 +1,10 @@
+const container=document.querySelector(".container")
+let paintColor="black"
+gridSize=16 
+
+
+
+
 function createGrid(){
     for( i=0;i<gridSize;i++){
 
@@ -13,5 +20,15 @@ function createGrid(){
         container.appendChild(line)
     }
 }
+
+
+
+
+container.addEventListener("mouseover",(e)=>{
+    
+    if(randomise)paintColor=`rgb(${Math.floor(Math.random() * 255) + 1},${Math.floor(Math.random() * 255) + 1},${Math.floor(Math.random() * 255) + 1})`
+    e.target.style.backgroundColor=paintColor
+})
+
 
 createGrid()
